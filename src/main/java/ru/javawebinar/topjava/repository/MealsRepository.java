3895fcb9441fb177944444a8692a5baa4b60b30b
020010898 <a.carasiov19@gmail.com>
2021-02-21 20:16:07 +0100

    hw1

---
+++ src/main/java/ru/javawebinar/topjava/repository/MealsRepository.java
@@ -0,0 +1,12 @@
+package ru.javawebinar.topjava.repository;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Collection;
+
+public interface MealsRepository {
+    Meal save(Meal meal);
+    void delete(Meal meal);
+    Meal get(int id);
+    Collection<Meal> getAll();
+}
