f1e06b3bc8c571261759b8c47a262beaec3eb2aa
020010898 <a.carasiov19@gmail.com>
2021-02-28 22:49:32 +0100

    hw02_1&

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,71 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
+        if(repository.get(id)!= null){
+            repository.remove(id);
+        }
         return true;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if(user.isNew()){
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            return user;
+        }
+        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
+        if(repository.get(id)!= null){
+            return repository.get(id);
+        }
         return null;
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        repository.put(1, new User(1, "Ivan", "ivan@mail.com", "123", Role.USER));
+        repository.put(2, new User(2, "Alex", "alex@mail.com", "222", Role.USER));
+
+        List<User> list =  repository.entrySet().stream().sorted(Comparator.comparing(e -> e.getValue().getName()))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+        return list;
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
+        for(Map.Entry<Integer, User> map : repository.entrySet()){
+            if(map.getValue().getEmail().equals(email)){
+                return repository.get(map.getKey());
+            }
+        }
         return null;
     }
 }
